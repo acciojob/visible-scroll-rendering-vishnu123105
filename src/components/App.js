aa2d7648dbf9edfcb527728ef5761d7a66d6d390
Vishnu <bandarivishnu143@gmail.com>
2025-05-18 12:37:10 +0530

    vishnu

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 
-import React from "react";
+import React, { useRef } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const arr = useRef(Array.from({length:15},()=>0))
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{height:"500px",overflowY:"scroll"}}>
+        {arr.current.map((_,i)=><Item n={i} key={i+1}/>)}
     </div>
   )
 }
+const Item=({n})=>(
+  <>
+  <h1>Item {n}</h1>
+  <p>Lorem iposum dolor sit amet</p>
+  </>
+)
 
 export default App
